b82abbb34c01c5e8c1061b95ef37109d53be4020
CFettig <claire.fettig@siu.edu>
2021-03-17 14:47:32 -0500

    vaguely restructured

--- client.py
+++ client.py
@@ -3,26 +3,40 @@
 import socket
 import sys
 
-if len(sys.argv) != 3:
-    print("usage: client.py <host-ip> <port-number>")
-    sys.exit(1)
 
-HOST = sys.argv[1]
-PORT = int(sys.argv[2])
+def conn_to_server(socket):
+    data = s.recv(2048).decode('utf-8')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
+    user_name = input(data)
+    s.sendall(bytes(user_name, 'utf-8'))
+    data = s.recv(2048).decode('utf-8')
 
-        data = s.recv(2048).decode('utf-8')
-
+    while 'Name already taken' in data:
         user_name = input(data)
         s.sendall(bytes(user_name, 'utf-8'))
         data = s.recv(2048).decode('utf-8')
-        while 'Name already taken' in data:
-            user_name = input(data)
-            s.sendall(bytes(user_name, 'utf-8'))
-            data = s.recv(2048).decode('utf-8')
-        print(data)
-        
 
-        # user_in = input('$>')
+    print(data)
+        
+def request_talk(ip):
+    s.sendall('ip')
+
+def accept_talk():
+    pass
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("usage: client.py <host-ip> <port-number>")
+        sys.exit(1)
+
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
+        conn_to_server(s)
+        print("Enter \'talk <user-ip>\' to talk to another user\nEnter exit to disconnect")
+        
+        while input('>') != 'exit':
+            print('hello')
--- server.py
+++ server.py
@@ -8,14 +8,24 @@ import threading
 HOST = '0.0.0.0'
 PORT = 65426
 
+#dict of clients
+#key: client name
+#value: [ip, port, logged_in]
 clients = {}
 
 def add_client(name, addr):
+    #new client to be added
     if name not in clients:
-        clients[name] = addr
-        return None
+        clients[name] = [addr[0], addr[1], True]
+        #return None
+
+    #returning client to be marked online
+    elif (name in clients) and (addr[0]==clients[name][0]):
+        clients[name][2] = True
+        return 0
+
+    #new client cannot use existing name
     else:
-        print(clients)
         return 1
 
 def on_client_conn(conn):
@@ -27,14 +37,26 @@ def on_client_conn(conn):
 
     added = add_client(client_name, addr)
     
-    while added:
-        print(clients)
+    while added==1:
         conn.sendall(b'Name already taken. Enter name: ')
         client_name = conn.recv(1024).decode('utf-8')
         added = add_client(client_name, addr)
     
-    conn.sendall(b'Welcome ' + client_name.encode('utf-8'))
+    if added==0:
+        conn.sendall(b'Welcome back, ' + client_name.encode('utf-8'))
+    else:
+        conn.sendall(b'Welcome, ' + client_name.encode('utf-8'))
 
+    data = conn.recv(1024).decode('utf-8')
+    while data:
+        if 'request:' in data:
+            pass
+        elif 'exit' in data:
+            clients[name][2] = False
+        else:
+            pass
+        
+        data = conn.recv(1024).decode('utf-8')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
