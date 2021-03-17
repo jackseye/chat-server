0b0613445690007f43ee36327f344ba010303b2d
CFettig <claire.fettig@siu.edu>
2021-03-17 12:30:43 -0500

    adds client to dict

--- client.py
+++ client.py
@@ -3,21 +3,26 @@
 import socket
 import sys
 
-if len(sys.argv) != 2:
-    print("usage: client.py host_name")
+if len(sys.argv) != 3:
+    print("usage: client.py <host-ip> <port-number>")
     sys.exit(1)
 
 HOST = sys.argv[1]
-PORT = 65429
+PORT = int(sys.argv[2])
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        
         s.connect((HOST, PORT))
 
         data = s.recv(2048).decode('utf-8')
 
         user_name = input(data)
         s.sendall(bytes(user_name, 'utf-8'))
-        print(s.recv(2048).decode('utf-8'))
+        data = s.recv(2048).decode('utf-8')
+        while 'Name already taken' in data:
+            user_name = input(data)
+            s.sendall(bytes(user_name, 'utf-8'))
+            data = s.recv(2048).decode('utf-8')
+        print(data)
+        
 
         # user_in = input('$>')
--- server.py
+++ server.py
@@ -6,33 +6,34 @@ import subprocess
 import threading
 
 HOST = '0.0.0.0'
-PORT = 65429
+PORT = 65426
 
 clients = {}
 
 def add_client(name, addr):
     if name not in clients:
         clients[name] = addr
+        return None
     else:
+        print(clients)
         return 1
 
 def on_client_conn(conn):
     print('connected by', addr)
-    while True:
-        print("connecting")
-        data = conn.recv(1024)
-        if not data:
-            break
-        print("connected")
+    
+    conn.sendall(b'Enter your name: ')
+    
+    client_name = conn.recv(1024).decode('utf-8')
 
-        conn.sendall(b'Enter your name: ')
-
+    added = add_client(client_name, addr)
+    
+    while added:
+        print(clients)
+        conn.sendall(b'Name already taken. Enter name: ')
         client_name = conn.recv(1024).decode('utf-8')
-        
-        if add_client(client_name, addr):
-            conn.sendall(b'Name already taken. Enter name: ')
-        else:
-            conn.sendall(b'Welcome ' + client_name.encode('utf-8'))
+        added = add_client(client_name, addr)
+    
+    conn.sendall(b'Welcome ' + client_name.encode('utf-8'))
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
